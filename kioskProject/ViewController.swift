63d336bd7476bd2576fa145f668cfe3d2b3d7c77
kseunghui@gmail.com <kseunghui@gmail.com>
2024-07-02 11:33:51 +0900

    Initial Commit

---
+++ kioskProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kioskProject
+//
+//  Created by 김승희 on 7/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
