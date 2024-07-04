5ec41f0526fd48bf50d8e65ad26fd4c207596b15
sh990920 <sh990920@gmail.com>
2024-07-04 17:28:48 +0900

    [Feat] MenuCollectionVeiw 기능추가 및 위치 조정

--- kioskProject/Controllers/MainViewController.swift
+++ kioskProject/Controllers/MainViewController.swift
@@ -7,11 +7,44 @@
 
 import UIKit
 import SnapKit
+import SwiftUI
 
 class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
+        let segmentedBar = SegmentedBar()
+        self.view.addSubview(segmentedBar)
+        segmentedBar.snp.makeConstraints {
+            $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+        }
     }
 
 }
+
+struct PreView: PreviewProvider {
+    static var previews: some View {
+        MainViewController().toPreview()
+    }
+}
+#if DEBUG
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+            let viewController: UIViewController
+
+            func makeUIViewController(context: Context) -> UIViewController {
+                return viewController
+            }
+
+            func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            }
+        }
+
+        func toPreview() -> some View {
+            Preview(viewController: self)
+        }
+}
+#endif
